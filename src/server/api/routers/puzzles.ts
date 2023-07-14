bd37463f7b1d4d3cf57db4612c965826087cfaa7
Nabil Spann <nabilspann@gmail.com>
2023-07-14 16:01:11 -0600

    Prisma query random chess game

--- src/server/api/routers/puzzles.ts
+++ src/server/api/routers/puzzles.ts
@@ -2,7 +2,18 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const puzzlesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.puzzle.findMany();
+  getOne: publicProcedure
+  .input(z.object({difficulty: z.string()}))
+  .query(async ({ ctx, input }) => {
+    const count = await ctx.prisma.puzzle.count({
+      where: { difficulty: input.difficulty },
+    });
+    const skip = Math.floor(Math.random() * count);
+
+    return ctx.prisma.puzzle.findMany({
+      skip,
+      take: 1,
+      where: { difficulty: input.difficulty },
+    });
   }),
 });
