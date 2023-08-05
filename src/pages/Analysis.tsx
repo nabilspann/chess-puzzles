57d495d99163b2b4ef02dba061d35b2f27b3d0f1
Nabil Spann <nabilspann@gmail.com>
2023-08-04 21:10:13 -0600

    Adding previous move logic

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -14,18 +14,19 @@ interface PropType {
 
 const Analysis = ({data, boardOrientation, anim} : PropType) => {
     const [game, setGame] = useState(new Chess());
-    const [currentMove, setCurrentMove] = useState({ moveCount: 0, boardOrientation });
-    console.log("boardOrientation", currentMove);
+    const [currentMove, setCurrentMove] = useState({ moveCount: 0, turn: boardOrientation, currentFen: game.fen() });
+
     useEffect(() => {
       const fen = data?.[0]?.fen;
       if (fen) {
         game.load(fen);
+        setCurrentMove({ ...currentMove, currentFen: fen})
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [data]);
     
     useEffect(() => {
-      setCurrentMove({ ...currentMove, boardOrientation });
+      setCurrentMove({ ...currentMove, turn: boardOrientation });
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [boardOrientation])
 
@@ -34,28 +35,63 @@ const Analysis = ({data, boardOrientation, anim} : PropType) => {
       () => formatPgn(puzzleData?.pgn || ""),
       [puzzleData?.pgn]
     );
+    const pgnLength = formattedPgn.length;
 
     const handleNextMove = () => {
       const pushMove =
-        formattedPgn?.[currentMove.moveCount]?.[currentMove.boardOrientation];
-      if(pushMove){
+        formattedPgn?.[currentMove.moveCount]?.[currentMove.turn];
+      console.log("currentFEn", currentMove);
+      console.log("game.fen()", game.fen());
+
+      console.log("fen equality?", game.fen() === currentMove.currentFen)
+      if(pushMove && currentMove.moveCount < pgnLength) {
         const { gameCopy } = makeAMove(
           pushMove,
           game
         );
+
         setGame(gameCopy);
-      }
-      if(currentMove.boardOrientation === WHITE){
-        const nextMove = { ...currentMove, boardOrientation: BLACK };
-        setCurrentMove(nextMove);
-      }
-      else if (currentMove.boardOrientation === BLACK) {
-        const nextMove = { moveCount: currentMove.moveCount + 1, boardOrientation: WHITE };
-        setCurrentMove(nextMove);
+        if (currentMove.turn === WHITE) {
+          const nextMove = { ...currentMove, turn: BLACK };
+          setCurrentMove(nextMove);
+        } else if (
+          currentMove.turn === BLACK &&
+          currentMove.moveCount < pgnLength - 1
+        ) {
+          const nextMove = {
+            moveCount: currentMove.moveCount + 1,
+            turn: WHITE,
+          };
+          setCurrentMove(nextMove);
+        }
       }
     };
 
-    console.log("formattedPgn", formattedPgn);
+    const handlePreviousMove = () => {
+      const gameCopy = { ...game };
+
+      const previousMove = gameCopy.undo();
+      if (currentMove.turn === WHITE && previousMove) {
+        const nextMove = {
+          moveCount: currentMove.moveCount - 1,
+          turn: BLACK,
+        };
+        setCurrentMove(nextMove);
+      } else if (
+        currentMove.turn === BLACK &&
+        !!previousMove &&
+        currentMove.moveCount > 0
+      ) {
+        const nextMove = {
+          moveCount: currentMove.moveCount,
+          turn: WHITE,
+        };
+        setCurrentMove(nextMove);
+      }
+      setGame(gameCopy);
+    }
+
+    console.log("currentMove", currentMove)
     return (
       <div className="p-5 pt-10 md:grid md:grid-cols-6">
         <div className="md:col-span-4 md:px-5">
@@ -66,7 +102,7 @@ const Analysis = ({data, boardOrientation, anim} : PropType) => {
             mutateGame={(newGame) => setGame(newGame)}
           />
           <div>
-            <button>Previous</button>
+            <button onClick={handlePreviousMove}>Previous</button>
             <button onClick={handleNextMove}>Next</button>
           </div>
         </div>
